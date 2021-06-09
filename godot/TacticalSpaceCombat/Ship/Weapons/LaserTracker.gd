4cb07ef9be3183eb9ee11b52bed8e7a6fe8a5a5b
Răzvan C. Rădulescu <razcore.art@gmail.com>
2021-06-09 19:32:01 +0300

    Sync with L15

--- godot/TacticalSpaceCombat/Ship/Weapons/LaserTracker.gd
+++ godot/TacticalSpaceCombat/Ship/Weapons/LaserTracker.gd
@@ -2,11 +2,9 @@ extends Node2D
 
 signal targeted(msg)
 
-const TARGET_LINE_DEFAULT := PoolVector2Array([Vector2.INF, Vector2.INF])
+const LINE_DEFAULT := PoolVector2Array([Vector2.INF, Vector2.INF])
 
-var _swipe_start := Vector2.ZERO
 var _is_targeting := false
-var _points := TARGET_LINE_DEFAULT
 var _targeting_length := 0
 var _rng := RandomNumberGenerator.new()
 var _rooms: Node2D = null
@@ -31,6 +29,8 @@ func setup(color: Color, rooms: Node2D, shield: Area2D) -> void:
 
 func _ready() -> void:
 	_rng.randomize()
+	target_line.points = LINE_DEFAULT
+	line.points = LINE_DEFAULT
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -38,13 +38,13 @@ func _unhandled_input(event: InputEvent) -> void:
 		return
 
 	if event.is_action_pressed("left_click"):
-		_points[0] = get_local_mouse_position()
-	elif _points[0] != Vector2.INF and event is InputEventMouseMotion:
-		var offset: Vector2 = get_local_mouse_position() - _points[0]
+		target_line.points[0] = get_local_mouse_position()
+	elif target_line.points[0] != Vector2.INF and event is InputEventMouseMotion:
+		var offset: Vector2 = get_local_mouse_position() - target_line.points[0]
 		offset = offset.clamped(_targeting_length)
-		_points[1] = _points[0] + offset
-		target_line.points = _points
-	elif event.is_action_released("left_click") and _points[1] != Vector2.INF:
+		target_line.points[1] = target_line.points[0] + offset
+		target_line.points = target_line.points
+	elif event.is_action_released("left_click") and target_line.points[1] != Vector2.INF:
 		_is_targeting = false
 		emit_signal("targeted", {"type": Controller.Type.LASER})
 
@@ -55,33 +55,34 @@ func _on_Controller_targeting(msg: Dictionary) -> void:
 			_is_targeting = is_targeting
 			_targeting_length = targeting_length
 			if _is_targeting:
-				_points = TARGET_LINE_DEFAULT
+				target_line.points = LINE_DEFAULT
 		{"targeting_length": var targeting_length}:
-			_points = _rooms.get_laser_points(targeting_length)
-			target_line.points = _points
+			target_line.points = _rooms.get_laser_points(targeting_length)
+			target_line.points = target_line.points
 			emit_signal("targeted", {"type": Controller.Type.LASER})
 
 
 func _on_Weapon_fire_started(params: Dictionary) -> void:
-	if _points[0] == Vector2.INF or _points[1] == Vector2.INF:
+	if target_line.points[0] == Vector2.INF or target_line.points[1] == Vector2.INF:
 		return
 
-	_swipe_start = _rooms.mean_position + Utils.randvf_circle(_rng, Projectile.MAX_DISTANCE)
 	area.set_deferred("monitorable", true)
 	area.params = params
-	tween.interpolate_method(self, "_swipe_laser", _points[0], _points[1], params.duration)
+
+	line.points[0] = _rooms.mean_position + Utils.randvf_circle(_rng, Projectile.MAX_DISTANCE)
+	tween.interpolate_method(self, "_swipe_laser", target_line.points[0], target_line.points[1], params.duration)
 	tween.start()
 
 
 func _on_Weapon_fire_stopped() -> void:
 	tween.remove_all()
+	line.points = LINE_DEFAULT
 	area.set_deferred("monitorable", false)
 	area.position = Vector2.ZERO
-	line.points = []
 
 
 func _swipe_laser(offset: Vector2) -> void:
-	line.points = [_swipe_start, offset]
+	line.points[1] = offset
 	if _shield != null and _shield.hitpoints > 0:
 		for points in Geometry.clip_polyline_with_polygon_2d(line.points, _shield_polygon):
 			line.points = points
